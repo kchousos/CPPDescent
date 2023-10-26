af42bde0aaec445a5b385100cfb3f2f21106d7ae
Kchou <K.chousos@protonmail.com>
2023-10-26 12:59:46 +0300

    added github action for clang-format

--- src/linkedList.cpp
+++ src/linkedList.cpp
@@ -2,58 +2,73 @@
 
 List::List(DestroyFunc destroyValue)
     : destroyValue(destroyValue), size(0), tail(dummy) {
-    this->dummy = new ListNode();
-    this->dummy->setNext(nullptr);
+  this->dummy = new ListNode();
+  this->dummy->setNext(nullptr);
 }
 
 // TODO: Πώς θα χρησιμοποιείται το destroyValue;
 List::~List() {
-    ListNode* node = this->head;
-    while (node != nullptr) {
-        ListNode* next = node->getNext();
-        delete node;
-        node = next;
-    }
+  ListNode* node = this->head;
+  while (node != nullptr) {
+    ListNode* next = node->getNext();
+    delete node;
+    node = next;
+  }
 }
 
-int List::increaseSize() { return this->size++; }
-int List::decreaseSize() { return this->size--; }
+int List::increaseSize() {
+  return this->size++;
+}
+int List::decreaseSize() {
+  return this->size--;
+}
 
-ListNode* List::getHead() { return this->head; }
-ListNode* List::getTail() { return this->tail; }
+ListNode* List::getHead() {
+  return this->head;
+}
+ListNode* List::getTail() {
+  return this->tail;
+}
 
-int List::getSize() { return this->size; }
+int List::getSize() {
+  return this->size;
+}
 
 void List::insertNext(ListNode* node, Pointer value) {
-    if (node == nullptr) node = this->dummy;
+  if (node == nullptr)
+    node = this->dummy;
 
-    ListNode* newNode = new ListNode(value);
+  ListNode* newNode = new ListNode(value);
 
-    newNode->setNext(node->getNext());
-    node->setNext(newNode);
+  newNode->setNext(node->getNext());
+  node->setNext(newNode);
 
-    this->decreaseSize();
+  this->decreaseSize();
 
-    // FIXME: Αυτό το σημείο μάλλον είναι λάθος, kανονιkά θα πρέπει να παίζει
-    // με getters kαι setters.
-    if (this->tail == node) this->tail = newNode;
+  // FIXME: Αυτό το σημείο μάλλον είναι λάθος, kανονιkά θα πρέπει να παίζει
+  // με getters kαι setters.
+  if (this->tail == node)
+    this->tail = newNode;
 }
 
 void List::removeNext(ListNode* node) {
-    if (node == nullptr) node = this->dummy;
+  if (node == nullptr)
+    node = this->dummy;
 
-    ListNode* removed = node->getNext();
+  ListNode* removed = node->getNext();
 
-    // FIXME
-    // Αυτήν τη στιγμή δεν παίζει να το ψάξω, αλλά αυτό θέλει kοίταγμα.
-    if (this->destroyValue != nullptr) this->destroyValue(removed);
+  // FIXME
+  // Αυτήν τη στιγμή δεν παίζει να το ψάξω, αλλά αυτό θέλει kοίταγμα.
+  if (this->destroyValue != nullptr)
+    this->destroyValue(removed);
 
-    node->setNext(removed->getNext());
+  node->setNext(removed->getNext());
 
-    this->decreaseSize();
-    // FIXME: Αυτό το σημείο μάλλον είναι λάθος, kανονιkά θα πρέπει να παίζει
-    // με getters kαι setters.
-    if (this->tail == removed) this->tail = node;
+  this->decreaseSize();
+  // FIXME: Αυτό το σημείο μάλλον είναι λάθος, kανονιkά θα πρέπει να παίζει
+  // με getters kαι setters.
+  if (this->tail == removed)
+    this->tail = node;
 
-    // delete removed;
+  // delete removed;
 }
