957cc1b5c475439f2c69d65f57b9695e4b7ea18a
Konstantinos Chousos <K.chousos@protonmail.com>
2023-10-27 12:13:12 +0300

    Documentation version number now corresponds to latest commit

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 cmake -S . -B build
 cmake --build build
-(cd docs; doxygen;)
-# (cd docs/latex; make refman.pdf)
+(cd docs; PROJECT_NUMBER="$(git rev-parse --short HEAD ; git diff-index --quiet HEAD || echo '(with uncommitted changes)')" doxygen;)
+(cd docs/latex; make refman.pdf)
 clang-format -i */*.cpp include/cppdescent/*.hpp
 ./build/app/app
