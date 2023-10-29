a9749896ac8dbb787eb32bf8af2e9567c4a1b879
Konstantinos Chousos <K.chousos@protonmail.com>
2023-10-29 20:41:03 +0200

    added code coverage

--- docs/html/search/defines_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['list_5fbof_0',['LIST_BOF',['../linkedList_8hpp.html#a2f739713417a71ea4bdbec824df269c6',1,'linkedList.hpp']]],
-  ['list_5feof_1',['LIST_EOF',['../linkedList_8hpp.html#af76a7efa59a6ee0a1c6f79662285c817',1,'linkedList.hpp']]]
-];
--- pre-commit.sh
+++ pre-commit.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+# clean build
+# rm -rf build/
 cmake -S . -B build
 cmake --build build
+
+# generate documentation
+rm -rf docs/html
+rm -rf docs/latex
 (cd docs; PROJECT_NUMBER="$(git rev-parse --short HEAD ; git diff-index --quiet HEAD || echo '(with uncommitted changes)')" doxygen;)
 (cd docs/latex; make refman.pdf)
+
+# format source code
 clang-format -i */*.cpp include/cppdescent/*.hpp
-./build/test/*.test
+
+# run tests
+for i in ./build/test/*.test 
+do 
+    $i
+done
+
+# # generate code coverage
+rm -f ./test/coverage.info ./test/filtered_coverage.info
+rm -rf docs/lcov
+lcov -c -d ./build/test/CMakeFiles/ -o ./test/coverage.info
+lcov --remove ./test/coverage.info "/usr/*" "$(pwd)/extern/googletest/*" -o ./test/filtered_coverage.info
+genhtml test/filtered_coverage.info --output-directory ./docs/lcov/
