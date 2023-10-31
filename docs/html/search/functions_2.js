ebdc72cc034c157cc07afa387bed85e949ffc450
Konstantinos Chousos <K.chousos@protonmail.com>
2023-10-31 10:24:27 +0200

    Regenerate docs

--- docs/html/search/functions_2.js
+++ docs/html/search/functions_2.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['getat_0',['getAt',['../classVector.html#a590a87df6746cd387df2524e2d832ab8',1,'Vector']]],
   ['gethead_1',['getHead',['../classList.html#a844cc92b5a773d0d97f505ff21bdec4a',1,'List']]],
-  ['getmax_2',['getMax',['../classPQueue.html#adc120c7a219f76a42395b1960527f4f3',1,'PQueue']]],
+  ['getmax_2',['getMax',['../classPQueue.html#abd46c02910bd2948a44d978b94ccd3fc',1,'PQueue']]],
   ['getnext_3',['getNext',['../classListNode.html#a3563e02bda689dc8780beac88b3ea7d3',1,'ListNode']]],
   ['getsize_4',['getSize',['../classList.html#a4dc0be43de595d393c22aad3248df0d8',1,'List::getSize()'],['../classPQueue.html#ac053881ae6f631ee4cbbcf93873ad2a5',1,'PQueue::getSize()'],['../classVector.html#afd223e84f6158ab4872bada1d71f66aa',1,'Vector::getSize()']]],
   ['gettail_5',['getTail',['../classList.html#a451bc8f2964e3ac355ba58578fb748ef',1,'List']]],
