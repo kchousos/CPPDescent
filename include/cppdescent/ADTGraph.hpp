aa9c5d6d9040fbe35b2becee0c206cc39822bc59
Konstantinos Chousos <K.chousos@protonmail.com>
2023-11-01 23:05:04 +0200

    vertexPair constructor

--- include/cppdescent/ADTGraph.hpp
+++ include/cppdescent/ADTGraph.hpp
@@ -44,6 +44,9 @@ class Graph {
 
 class GraphVertexPair {
  public:
+  GraphVertexPair(Graph* owner, Pointer vertex1, Pointer vertex2)
+      : owner(owner), vertex1(vertex1), vertex2(vertex2){};
+  ~GraphVertexPair();
   Pointer getVertex1() { return this->vertex1; };
   Pointer getVertex2() { return this->vertex2; };
   Graph* getOwner() { return this->owner; };
--- src/ADTGraph.cpp
+++ src/ADTGraph.cpp
@@ -8,3 +8,23 @@
  * @copyright Copyright (c) 2023
  *
  */
+
+#include "cppdescent/ADTGraph.hpp"
+
+int* createInt(int value) {
+  int* p = new int;
+  *p = value;
+  return p;
+}
+
+int compareVertexPair(GraphVertexPair* pair1, GraphVertexPair* pair2) {
+  if (pair1->getOwner()->getCompare()(pair1->getVertex1(), pair2->getVertex1()))
+    return pair1->getOwner()->getCompare()(pair1->getVertex1(),
+                                           pair2->getVertex1());
+  else if (pair1->getOwner()->getCompare()(pair1->getVertex2(),
+                                           pair2->getVertex2()))
+    return pair1->getOwner()->getCompare()(pair1->getVertex2(),
+                                           pair2->getVertex2());
+
+  return 0;
+}
