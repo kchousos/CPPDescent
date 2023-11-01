b5164e6a7dbf17c6ee740393f4b1c010893f9af4
Phaedon Seitanidis <fedonseitan@gmail.com>
2023-11-02 00:14:43 +0200

    Graph test formatted

--- test/unit/ADTGraph.test.cpp
+++ test/unit/ADTGraph.test.cpp
@@ -15,7 +15,6 @@
 #include "cppdescent/ADTList.hpp"
 #include "gtest/gtest.h"
 
-
 /**
  * @brief A simple function to compare 2 integer pointers.
  *
@@ -29,7 +28,7 @@ int compareInts(Pointer a, Pointer b) {
 
 /**
  * @brief Delete an int pointer.
- * 
+ *
  * @param value The pointer to be deleted.
  */
 void deleteInts(Pointer value) {
@@ -50,79 +49,80 @@ int* createInt(int value) {
 
 /**
  * @brief Cast to size_t that has the same length as a Pointer
- * 
+ *
  * @param value The value to be hashed.
  * @return unsigned int The hash value.
  */
 unsigned int hashPointer(Pointer value) {
-	return (size_t)value;
+  return (size_t)value;
 }
 
-
 TEST(ADTGraphTest, create) {
-    Graph* graph = new Graph(compareInts, nullptr);
+  Graph* graph = new Graph(compareInts, nullptr);
 
-    ASSERT_NE(graph, nullptr);
-    ASSERT_EQ(graph->getSize(), 0);
+  ASSERT_NE(graph, nullptr);
+  ASSERT_EQ(graph->getSize(), 0);
 
-    delete graph;
+  delete graph;
 }
 
 TEST(ADTGraphTest, insertRemove) {
-    Graph* graph = new Graph(compareInts, nullptr);
+  Graph* graph = new Graph(compareInts, nullptr);
 
-    graph->setHashFunction(hashPointer);
-    int N = 1000;
+  graph->setHashFunction(hashPointer);
+  int N = 1000;
 
-    int** vertexArray = new int*[N];
+  int** vertexArray = new int*[N];
 
-    for (int i = 0; i < N; i++) {
-        vertexArray[i] = createInt(i);
-        graph->insertVertex(vertexArray[i]);
+  for (int i = 0; i < N; i++) {
+    vertexArray[i] = createInt(i);
+    graph->insertVertex(vertexArray[i]);
 
-        ASSERT_EQ(graph->getSize(), i+1);
-    }
+    ASSERT_EQ(graph->getSize(), i + 1);
+  }
 
-    List* list = graph->getVertices();
-    ListNode* node = list->getHead();
+  List* list = graph->getVertices();
+  ListNode* node = list->getHead();
 
-    for (int i = 0; i < N; i++) {
-        ASSERT_EQ(node->getValue(), vertexArray[i]);
-        node->getNext();
-    }
+  for (int i = 0; i < N; i++) {
+    ASSERT_EQ(node->getValue(), vertexArray[i]);
+    node->getNext();
+  }
 
-    for (int i = 0; i < N; i++) {
-        graph->removeVertex(vertexArray[i]);
-        ASSERT_EQ(graph->getSize(), N-i-1);
-    }
+  for (int i = 0; i < N; i++) {
+    graph->removeVertex(vertexArray[i]);
+    ASSERT_EQ(graph->getSize(), N - i - 1);
+  }
 
-    list = graph->getVertices();
-    ASSERT_EQ(list->getSize(), 0);
+  list = graph->getVertices();
+  ASSERT_EQ(list->getSize(), 0);
 
-    delete[] vertexArray;
-    delete list;
-    delete graph;
+  delete[] vertexArray;
+  delete list;
+  delete graph;
 }
 
 TEST(ADTGraphTest, getAdjacent) {
-    Graph* graph = new Graph(compareInts, deleteInts);
+  Graph* graph = new Graph(compareInts, deleteInts);
 
-    graph->setHashFunction(hashPointer);
+  graph->setHashFunction(hashPointer);
 
-    int N = 1000;
+  int N = 1000;
 
-    int** vertexArray = new int*[N];
+  int** vertexArray = new int*[N];
 
-    for (int i = 0; i < N; i++) {
-        vertexArray[i] = createInt(i);
-        graph->insertVertex(vertexArray[i]);
-    }
+  for (int i = 0; i < N; i++) {
+    vertexArray[i] = createInt(i);
+    graph->insertVertex(vertexArray[i]);
+  }
 
-    for (int i = 0; i < N-1; i = i+2) {
-        graph->insertEdge(vertexArray[i], vertexArray[i+1], i);
-        ASSERT_TRUE(graph->getWeight(vertexArray[i], vertexArray[i+1]) == i || graph->getWeight(vertexArray[i], vertexArray[i+1]) == INT_MAX);
-    }
+  for (int i = 0; i < N - 1; i = i + 2) {
+    graph->insertEdge(vertexArray[i], vertexArray[i + 1], i);
+    ASSERT_TRUE(graph->getWeight(vertexArray[i], vertexArray[i + 1]) == i ||
+                graph->getWeight(vertexArray[i], vertexArray[i + 1]) ==
+                    INT_MAX);
+  }
 
-    delete[] vertexArray;
-    delete graph;
+  delete[] vertexArray;
+  delete graph;
 }
