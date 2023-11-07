426e4596a402985dbc24b5394d23e7d9dd87cf43
Konstantinos Chousos <K.chousos@protonmail.com>
2023-11-07 18:30:14 +0200

    testing gh action tests

--- test/unit/cppdescent.test.cpp
+++ test/unit/cppdescent.test.cpp
@@ -97,7 +97,8 @@ TEST(IO, readData) {
   float lastValue = 0.0726192221;
 
   Vector* lastElement = (Vector*)vec->getAt(vec->getSize() - 1);
-  ASSERT_EQ(lastValue, *(float*)lastElement->getAt(lastElement->getSize() - 1));
+  ASSERT_FLOAT_EQ(lastValue,
+                  *(float*)lastElement->getAt(lastElement->getSize() - 1));
 
   int result = cppdescent::deleteDatapointVectors(vec);
   ASSERT_EQ(result, 0);
