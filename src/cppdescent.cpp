3cb24e025626e53ef1071f69252418c43eb9bd62
Kchou <K.chousos@protonmail.com>
2023-12-02 20:12:28 +0200

    moved metrics to cppdescent

--- src/cppdescent.cpp
+++ src/cppdescent.cpp
@@ -9,6 +9,7 @@
  *
  */
 #include "cppdescent/cppdescent.hpp"
+#include <cmath>
 #include <cstdint>
 #include <iostream>
 
@@ -484,4 +485,75 @@ List* NNDescent_Query(Graph* graph, int K, CompareFunc compare, Vector* query) {
   }
 
   delete vertices;
+}
+
+// ============================ Metric Functions =============================
+
+float cppdescent::euclideanDistance(Pointer a, Pointer b) {
+  Vector* first = (Vector*)a;
+  Vector* second = (Vector*)b;
+  float result = 0;
+
+  if (first->getSize() != second->getSize())
+    return -1.0;
+
+  for (int i = 0; i < first->getSize(); i++) {
+    float diff = *(float*)first->getAt(i) - *(float*)second->getAt(i);
+    result += diff * diff;
+  }
+
+  result = sqrtf(result);
+  return result;
+}
+
+float cppdescent::manhattanDistance(Pointer a, Pointer b) {
+  Vector* first = (Vector*)a;
+  Vector* second = (Vector*)b;
+  float result = 0;
+
+  if (first->getSize() != second->getSize())
+    return -1.0;
+
+  for (int i = 0; i < first->getSize(); i++) {
+    float diff = *(float*)first->getAt(i) - *(float*)second->getAt(i);
+    result += fabs(diff);
+  }
+
+  return result;
+}
+
+int cppdescent::compareEdgesEuclidean(Pointer first, Pointer second) {
+  GraphVertexPair* pair1 = (GraphVertexPair*)first;
+  GraphVertexPair* pair2 = (GraphVertexPair*)second;
+
+  float a = euclideanDistance((Vector*)pair1->getVertex1(),
+                              (Vector*)pair1->getVertex2());
+  float b = euclideanDistance((Vector*)pair2->getVertex1(),
+                              (Vector*)pair2->getVertex2());
+
+  int value = 0;
+  if (b > a) {
+    value = -1;
+  } else if (a > b) {
+    value = 1;
+  }
+  return value;
+}
+
+int cppdescent::compareEdgesManhattan(Pointer first, Pointer second) {
+  GraphVertexPair* pair1 = (GraphVertexPair*)first;
+  GraphVertexPair* pair2 = (GraphVertexPair*)second;
+
+  float a = manhattanDistance((Vector*)pair1->getVertex1(),
+                              (Vector*)pair1->getVertex2());
+  float b = manhattanDistance((Vector*)pair2->getVertex1(),
+                              (Vector*)pair2->getVertex2());
+
+  int value = 0;
+  if (b > a) {
+    value = -1;
+  } else if (a > b) {
+    value = 1;
+  }
+  return value;
 }
