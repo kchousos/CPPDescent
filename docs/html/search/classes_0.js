8255fde344b697cd0367422f2b58bd0925c109b5
Konstantinos Chousos <K.chousos@protonmail.com>
2023-12-05 05:00:37 +0200

    Regenerate docs

--- docs/html/search/classes_0.js
+++ docs/html/search/classes_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['graph_0',['Graph',['../classGraph.html',1,'']]],
-  ['graphvertexpair_1',['GraphVertexPair',['../classGraphVertexPair.html',1,'']]]
+  ['graphvertex_1',['GraphVertex',['../classGraphVertex.html',1,'']]],
+  ['graphvertexpair_2',['GraphVertexPair',['../classGraphVertexPair.html',1,'']]]
 ];
